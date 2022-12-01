e5320049ce71d49555a7857b9313babb5f014013
tetorgal <hector.galarza.css@gmail.com>
2022-11-30 23:23:45 -0600

    v1

---
+++ contacto.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Soy el contacto 😉</h1>
+    <h2>soy la modificacion desde escritorio 👀</h2>
+    <h1>modificacion local </h1>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Soy un index 😁</h1>
+    <h1>Soy un index 😁😘</h1>
+    
+    <h1>Soy una modificacion remota</h1>
+    <h1>modificacion VSCode local</h1>
+</body>
+</html>
